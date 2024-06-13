3e452a60063e271c5b04eb1d0c4e351f17a2389d
Jose Amaya <syntaxter@outlook.com>
2024-06-12 20:42:41 -0500

    Post in production

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		let data:any =  request;
+		return new Response(JSON.stringify(data));
 	},
 };
